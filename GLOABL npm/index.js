f282c94fa1c8fd99ce4af8c3ca8a86f0def4e392
DakshDev <dakshkishore99@gmail.com>
2025-03-29 19:44:10 +0500

    Global Package cmd

---
+++ GLOABL npm/index.js
@@ -0,0 +1,18 @@
+// Global npm package cmds
+
+
+
+// npm i -g <package-name>
+// npm outdated -g
+// npm update -g <package-name>
+// npm remove -g <package-name>
+
+
+
+
+
+
+
+
+// what is dev dependes 
+// we can use dev-dependes for the development time not in the production
