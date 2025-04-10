f48cbab60ce0066ed211bef8cc21b2558fb69b64
DakshDev <dakshkishore99@gmail.com>
2025-04-11 00:54:42 +0500

    cli todo app

--- PATH Module/9 path_module.js
+++ PATH Module/9 path_module.js
@@ -4,6 +4,7 @@ let path = require("path");
 // console.log(__filename) // full path of the file
 
 let pathName = __dirname;
+console.log(pathName);
 
 let jsonFilePath = path.join(pathName,"user.json").basename;
 console.log(path.dirname(pathName)) // this
